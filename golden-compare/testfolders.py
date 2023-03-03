cda0a24334b0c85afb1ca4f2bfa9ebe389ce0881
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-03 13:27:44 +0530

    Update testfolders.py

--- golden-compare/testfolders.py
+++ golden-compare/testfolders.py
@@ -1,14 +1,16 @@
 from compare_folder import *
 import unittest
 
+
 class TestFolder(unittest.TestCase):
     def test_for_additional_files(self):
-        _,additionalfiles=compare_folder_contents(folderpath1,standardfile)
-        # self.assertEqual(len(missingfiles),0)
+        _, additionalfiles = compare_folder_contents(folderpath1, standardfile)
+        # self.assertEqual(len(missingfiles), 0)
         self.assertTrue(len(additionalfiles)>0)
+        
     def test_for_missing_files(self):
-        missingfiles,_=compare_folder_contents(folderpath2,standardfile)
-        # self.assertEqual(len(additionalfiles),0)
+        missingfiles, _ = compare_folder_contents(folderpath2, standardfile)
+        # self.assertEqual(len(additionalfiles), 0)
         self.assertTrue(len(missingfiles)>0)
            
 if __name__ == '__main__':
