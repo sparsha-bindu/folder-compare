58ad7e8f62d734dc1967d47117608d1286c9faf9
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-03 12:48:39 +0530

    Update compare_folder.py

--- golden-compare/compare_folder.py
+++ golden-compare/compare_folder.py
@@ -1,17 +1,18 @@
 import os
 import json
 
-standardfile="standardfile.json"
-folderpath1="."
-folderpath2="envfiles"
+standardfile= "standardfile.json"
+folderpath1= "."
+folderpath2= "envfiles"
 
-def compare_folder_contents(directory_path,standard_file):
-    fileObject=open(standard_file,"r")
-    jsoncontent=fileObject.read()
-    standardfilelist=json.loads(jsoncontent)
+
+def compare_folder_contents(directory_path, standard_file):
+    fileObject= open(standard_file,"r")
+    jsoncontent= fileObject.read()
+    standardfilelist= json.loads(jsoncontent)
     fileObject.close()
-    compared_files=[]
-    additional_files=[]
+    compared_files= []
+    additional_files= []
     for root,dirnames,filenames in os.walk(directory_path):
         for filename in filenames:
             if filename in standardfilelist:
