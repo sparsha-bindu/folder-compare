159ff8ffcc56f8f63c098ad6d3f447b3988db7b2
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-03 21:39:20 +0530

    Update compare_folder.py

--- golden-compare/compare_folder.py
+++ golden-compare/compare_folder.py
@@ -1,9 +1,7 @@
 import os
 import json
-
 standardfile = "standardfile.json"
-folderpath = "."
-# folderpath = "envfiles"
+folderpath = "folder_missing_files"
 
 
 def load_contents_of_standardfile(standardfilename):
@@ -36,7 +34,56 @@ def compare_folder_contents(directory_path, standard_file):
     allfiles = []
     for root, dirnames, filenames in os.walk(directory_path):
         for filename in filenames:
-            allfiles.append(filename)
+            fullpath = os.path.join(root,filename)
+            _, subpath = fullpath.split("\\",1)
+            allfiles.append(subpath)
     compared_files, additional_files = compared_n_additionalfiles(allfiles, standardfilelist)
+    print(compared_files)
+    print(additional_files)
     missing_files = missingfileslist(standardfilelist, compared_files)
+    print(missing_files)
     return missing_files, additional_files
+
+compare_folder_contents(folderpath,standardfile)
+# import os
+# import json
+
+# standardfile = "standardfile.json"
+# folderpath = "."
+# # folderpath = "envfiles"
+
+
+# def load_contents_of_standardfile(standardfilename):
+#     fileObject = open(standardfilename, "r")
+#     jsoncontent = fileObject.read()
+#     standardfilelist = json.loads(jsoncontent)
+#     fileObject.close()
+#     return standardfilelist
+
+
+# def missingfileslist(standardfilelist, compared_files):
+#     missing_files = []
+#     [missing_files.append(file) for file in standardfilelist if file not in compared_files]
+#     return missing_files
+
+
+# def compared_n_additionalfiles(allfiles, standardfilelist):
+#     compared_files = []
+#     additional_files = []
+#     for filename in allfiles:
+#         if filename in standardfilelist:
+#             compared_files.append(filename)
+#         else:
+#             additional_files.append(filename)
+#     return compared_files, additional_files
+
+
+# def compare_folder_contents(directory_path, standard_file):
+#     standardfilelist = load_contents_of_standardfile(standard_file)
+#     allfiles = []
+#     for root, dirnames, filenames in os.walk(directory_path):
+#         for filename in filenames:
+#             allfiles.append(filename)
+#     compared_files, additional_files = compared_n_additionalfiles(allfiles, standardfilelist)
+#     missing_files = missingfileslist(standardfilelist, compared_files)
+#     return missing_files, additional_files
