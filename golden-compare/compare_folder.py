a6584a152d52401502f7b20693ae475b70fee0e4
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-04 23:10:44 +0530

    Update compare_folder.py

--- golden-compare/compare_folder.py
+++ golden-compare/compare_folder.py
@@ -35,8 +35,8 @@ def compare_folder_contents(directory_path, standard_file):
     for root, dirnames, filenames in os.walk(directory_path):
         for filename in filenames:
             fullpath = os.path.join(root, filename)
-            _, subpath = fullpath.split("/", 1)
-            allfiles.append(subpath)
+#             _, subpath = fullpath.split("/", 1)
+            allfiles.append(fullpath)
     compared_files, additional_files = compared_n_additionalfiles(allfiles, standardfilelist)
     missing_files = missingfileslist(standardfilelist, compared_files)
     return missing_files, additional_files
